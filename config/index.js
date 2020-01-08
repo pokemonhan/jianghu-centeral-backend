a83bfef229df24032615c55c53763ca63e8c99af
aaron <h956688709@gmail.com>
2020-01-08 14:52:18 +0800

    改回绝对路径, 并打开history

--- config/index.js
+++ config/index.js
@@ -53,7 +53,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/', // 改回绝对路径
 
     /**
      * Source Maps
